2a3d6186c4c3522c1efd59affb64e33101646c5b
tsouris <nikolastsouris16@gmail.com>
2023-10-05 21:14:19 +0300

    Completed task 3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,100 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net.Sockets;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NetworPr_Client
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private TcpClient client;
+        private StreamReader reader;
+        private StreamWriter writer;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (client == null || !client.Connected)
+                {
+                    client = new TcpClient();
+                    await client.ConnectAsync(tbIp.Text, int.Parse(tbPort.Text));
+
+                    NetworkStream stream = client.GetStream();
+                    reader = new StreamReader(stream);
+                    writer = new StreamWriter(stream) { AutoFlush = true };
+                    btnGetQuote.IsEnabled = true;
+                    btnConnect.IsEnabled = false;
+
+                    Log("Connected to server.");
+                }
+                else
+                {
+                    Log("Already connected to the server.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log($"Error: {ex.Message}");
+            }
+        }
+
+        private async void GetQuote_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await writer.WriteLineAsync("GET_QUOTE");
+                string response = await reader.ReadLineAsync();
+
+                if (response == "MAX_QUOTES_REACHED")
+                {
+                    Log("Maximum number of quotes reached. Disconnecting...");
+                    Disconnect();
+                }
+                else
+                {
+                    DisplayQuote(response);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log($"Error: {ex.Message}");
+            }
+        }
+
+        private void DisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            Disconnect();
+        }
+
+        private void Disconnect()
+        {
+            try
+            {
+                client?.Close();
+                Log("Disconnected from server.");
+                btnGetQuote.IsEnabled = false;
+                btnConnect.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                Log($"Error: {ex.Message}");
+            }
+        }
+
+        private void DisplayQuote(string quote)
+        {
+            tbReceivedQuotes.Text += $"{quote}\n";
+        }
+
+        private void Log(string message)
+        {
+            tbLog.Text += $"{DateTime.Now} - {message}\n";
         }
     }
 }
